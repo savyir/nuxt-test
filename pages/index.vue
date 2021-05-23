9e4ca4018f31097dc49af63350f2961866d85069
savvy <savvyversa@gmail.com>
2021-05-23 16:04:01 +0430

    update vsp version

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
   <v-container style="margin-top: -80px;">
+    <h1 class="green--text text--darken-4 font-13">Vuetify and Strapi Tools</h1>
     <v-card rounded class="pa-5">
-      <vsp-reserve-item-list :items="items"/>
+      <vsp-reserve-item-list :items="items" sm="6" md="6" xl="6"/>
     </v-card>
   </v-container>
 </template>
@@ -12,7 +13,7 @@
       subTitle: 'open-source nuxt module',
       img: '/vsd.png',
       bookmark: false,
-      tagImages:[
+      tagImages: [
         "https://badge.fury.io/js/vuetify-strapi-dashboard.png"
       ],
       rows: [
@@ -35,14 +36,14 @@
       subTitle: 'open-source nuxt module',
       img: '/vsp.png',
       bookmark: false,
-      tagImages:[
+      tagImages: [
         "https://badge.fury.io/js/vuetify-strapi-page.png"
       ],
       rows: [
         {icon: "flag", title: 'landing pages'},
         {icon: "star", title: 'login and register pages'},
         {icon: "check", title: 'e-commerce'},
-        {icon: "money", title: 'strapi'},
+        {icon: "money", title: 'contact pages'},
       ],
       tags: [
         {icon: "flag", title: 'i18n'},
@@ -55,10 +56,10 @@
     },
     {
       title: 'Public Relation Panel',
-      subTitle: 'share and track contents in telegram, twitter, youtube etc.',
+      subTitle: 'share and track your contents in telegram, twitter, youtube, blog and other social platforms. build video from audio and image files and share as video.',
       img: '/pr-2.png',
       price: 25,
-      discount: 2,
+      discount: 5,
       unit: '$',
       stars: 5,
       rows: [
@@ -72,7 +73,29 @@
         {icon: "star", title: 'strapi', value: 'مقدار دوم'},
         {icon: "check", title: 'laravel', value: 'مقدار سوم'},
       ],
-      to: 'PR tool'
+      to: 'public-relation-javascript-cms'
+    },
+    {
+      title: 'Advanced Twitter Follower Controll',
+      subTitle: 'automated system for your twitter account. see your followers and unfollowers and mute, block, favorite your friends. build white-list, black-list and favorite-list.',
+      img: '/twitter.png',
+      price: 15,
+      discount: 5,
+      unit: '$',
+      stars: 5,
+      rows: [
+        {icon: "flag", title: 'unfollowers'},
+        {icon: "star", title: 'automatic favorite your friends'},
+        {icon: "check", title: 'white-list and black-list'},
+        {icon: "check", title: 'block, mute, unfollow actions'},
+      ],
+      tags: [
+        {icon: "flag", title: 'nuxt', value: 'مقدار اول'},
+        {icon: "star", title: 'strapi', value: 'مقدار دوم'},
+        {icon: "check", title: 'laravel', value: 'مقدار سوم'},
+        {icon: "check", title: 'twitter', value: 'مقدار سوم'},
+      ],
+      to: 'twitter-manage-followers-javascript-cms'
     },
   ];
   export default {
